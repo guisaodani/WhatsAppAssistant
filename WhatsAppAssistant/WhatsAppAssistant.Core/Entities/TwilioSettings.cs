c07abc9fbb5d98185534106cfd0c5919f5fcb21e
guisaodani <guisaodani@gmail.com>
2026-05-26 22:35:19 -0500

    agregar ITwilioService, TwilioSettings y TwilioService en Infrastructure

---
+++ WhatsAppAssistant/WhatsAppAssistant.Core/Entities/TwilioSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatsAppAssistant.Core.Entities
+{
+    public class TwilioSettings
+    {
+        public string AccountSid { get; set; } = string.Empty;
+        public string AuthToken { get; set; } = string.Empty;
+        public string WhatsAppNumber { get; set; } = string.Empty;
+    }
+}
---
+++ WhatsAppAssistant/WhatsAppAssistant.Infrastructure/Twilio/TwilioService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Options;
+using Twilio.Rest.Api.V2010.Account;
+using WhatsAppAssistant.Core.Entities;
+using WhatsAppAssistant.Core.Interfaces;
+using TwilioClient = Twilio.TwilioClient;
+using PhoneNumber = Twilio.Types.PhoneNumber;
+using IOptions = Microsoft.Extensions.Options.IOptions<WhatsAppAssistant.Core.Entities.TwilioSettings>;
+
+namespace WhatsAppAssistant.Infrastructure.Twilio;
+
+public class TwilioService : ITwilioService
+{
+    private readonly TwilioSettings _settings;
+
+    public TwilioService(IOptions settings)
+    {
+        _settings = settings.Value;
+        TwilioClient.Init(_settings.AccountSid, _settings.AuthToken);
+    }
+
+    public async Task SendMessageAsync(string to, string message)
+    {
+        await MessageResource.CreateAsync(
+            from: new PhoneNumber(_settings.WhatsAppNumber),
+            to: new PhoneNumber(to),
+            body: message
+        );
+    }
+}
